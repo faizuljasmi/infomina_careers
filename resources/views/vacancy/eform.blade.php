25dca23e369ade691e7add79b92c4c81d631615d
jasmifaizul <jasmifaizul@gmail.com>
2023-02-16 11:06:10 +0800

    hover fix

--- resources/views/vacancy/eform.blade.php
+++ resources/views/vacancy/eform.blade.php
@@ -84,7 +84,7 @@
                                                                                                                                                                                                                         NIK (IND): 320000564XXXXXXX <br>         
                                                                                                                                                                                                                         National ID No. (TH): 5 0499 40598 45 0 <br>
                                                                                                                                                                                                                         National ID No. (PH): 1234-5678-9101-1213
-                                                                                                                                                                                                                         " data-html="true" data-trigger="click"></i></label>
+                                                                                                                                                                                                                         " data-html="true" data-trigger="focus"></i></label>
                                                             <input type="text" oninput="this.value = this.value.replace(/[^0-9a-zA-Z-\s]/g, '')" class="form-control" id="applicant_ic" name="applicant_ic" placeholder="ID No." required>
 
                                                             <div class="invalid-feedback">
@@ -661,6 +661,9 @@
     $(document).ready(function() {
             demo.checkFullPageBackgroundImage();
             $('[data-toggle="popover"]').popover();
+            $('.popover-dismiss').popover({
+                trigger: 'focus'
+                });
         });
 function checkGender(){
     var x = document.getElementById("applicant_gender").value;
